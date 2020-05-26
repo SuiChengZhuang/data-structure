8f536cfeb3e84d631dcf4a0a3c55b15718fd2673
suichengzhuang <oneline1387@yeah.net>
2020-05-27 00:23:46 +0800

    修改包结构

---
+++ src/com/scz/circlequque/CircleQueue.java
@@ -0,0 +1,93 @@
+package com.scz.sparsearray.circlequque;
+
+import java.util.Scanner;
+
+/**
+ * @description: 环形队列
+ * @author: sui.chengZhuang
+ * @create: 2020-05-24 09:26
+ **/
+public class CircleQueue {
+
+    // 头指针 , 指向第一个元素
+    private int front;
+
+    // 尾指针 , 指向最后一个元素 , 约定最后一个位置空着
+    private int rear;
+
+    //最大容量
+    private int maxSize;
+
+    private int[] queue;
+
+    public CircleQueue(int maxSize) {
+        this.maxSize = maxSize;
+        this.queue = new int[maxSize];
+    }
+
+    public void show() {
+        for (int i = front; i < getQueueLength(); i++) {
+            if (i == front) {
+                System.out.print("queue : [" + queue[i % maxSize]);
+            } else {
+                System.out.print("," + queue[i % maxSize]);
+            }
+        }
+        if (isEmpty()) {
+            System.out.println("队列为空");
+        } else {
+            System.out.print("]");
+            System.out.println();
+        }
+    }
+
+    private int getQueueLength() {
+        return (rear + maxSize - front) % maxSize + front;
+    }
+
+    public void pop() {
+        if (isEmpty()) {
+            System.out.println("队列中已经没有元素,不能再移除");
+            return;
+        }
+
+        front = (front + 1) % maxSize;
+    }
+
+    private boolean isEmpty() {
+        return front == rear;
+    }
+
+    public void add(int i) {
+        if (isFull()) {
+            System.out.println("队列已满,不能再添加元素!");
+            return;
+        }
+        queue[rear] = i;
+        rear = (rear + 1) % maxSize;
+    }
+
+    private boolean isFull() {
+        return (rear + 1 - front) % maxSize == 0;
+    }
+
+    public static void main(String[] args) {
+        CircleQueue queue = new CircleQueue(5);
+        while (true) {
+            System.out.println("请输入需要操作的指令");
+            String command = new Scanner(System.in).next();
+            switch (command) {
+                case "a":
+                    System.out.println("请输入一个需要添加数字");
+                    queue.add(new Scanner(System.in).nextInt());
+                    break;
+                case "d":
+                    queue.pop();
+                    break;
+                case "s":
+                    queue.show();
+            }
+        }
+    }
+
+}
---
+++ src/com/scz/singlelinked/SingleLinkedDemo.java
@@ -0,0 +1,167 @@
+package com.scz.sparsearray.singlelinked;
+
+/**
+ * @description: 单链表
+ * @author: sui.chengZhuang
+ * @create: 2020-05-26 06:56
+ **/
+public class SingleLinkedDemo {
+    public static void main(String[] args) {
+        SingleLinked singleLinked = new SingleLinked();
+        SingleLinked.HeroNode node = new SingleLinked.HeroNode(1, "宋江", "及时雨");
+        SingleLinked.HeroNode node2 = new SingleLinked.HeroNode(2, "卢俊义", "玉麒麟");
+        SingleLinked.HeroNode node3 = new SingleLinked.HeroNode(3, "林冲", "豹子头");
+        SingleLinked.HeroNode node4 = new SingleLinked.HeroNode(4, "吴用", "智多星");
+
+        /* System.out.println("测试添加 1 2 3 4~~~");
+        singleLinked.add(node);
+        singleLinked.add(node2);
+        singleLinked.add(node3);
+        singleLinked.add(node4);
+        singleLinked.show();
+
+
+        System.out.println("测试删除 2 3~~~");
+        singleLinked.delete(2);
+        singleLinked.delete(3);
+        singleLinked.delete(4);
+        singleLinked.show();
+
+        System.out.println("测试修改 1 ~~~");
+        singleLinked.modify(new SingleLinked.HeroNode(1, "晁盖", "晁天王"));
+        singleLinked.show();
+           */
+        System.out.println("测试有序添加~~~");
+        singleLinked.addSort(node2);
+        singleLinked.addSort(node3);
+        singleLinked.addSort(node);
+        singleLinked.addSort(new SingleLinked.HeroNode(7, "武松", "转治西门庆"));
+        singleLinked.addSort(node4);
+
+        singleLinked.show();
+    }
+}
+
+class SingleLinked {
+
+    private HeroNode head = new HeroNode(0, "", "");
+
+    public void addSort(HeroNode node) {
+        HeroNode temp = this.head;
+        while (temp.next != null) {
+            if (temp.next.getNo() == node.getNo()) {
+                throw new RuntimeException("重复添加排名为" + node.getNo() + "的英雄");
+            }
+
+            if (node.getNo() > temp.getNo() && node.getNo() < temp.next.getNo()) {
+                node.next = temp.next;
+                temp.next = node;
+                return;
+            }
+            temp = temp.next;
+        }
+        temp.next = node;
+    }
+
+    public void add(HeroNode node) {
+        HeroNode temp = this.head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = node;
+    }
+
+    public void delete(int no) {
+        HeroNode temp = this.head;
+        while (temp.next != null) {
+            if (temp.next.getNo() == no) {
+                temp.next = temp.next.next;
+                return;
+            }
+            temp = temp.next;
+        }
+
+        if (temp.getNo() != no) {
+            System.out.println("未找到排名为" + no + "的英雄");
+        }
+    }
+
+    private HeroNode find(int no) {
+        HeroNode temp = this.head;
+        while (temp.next != null) {
+            if (temp.next.getNo() == no) {
+                return temp.next;
+            }
+            temp = temp.next;
+        }
+        throw new RuntimeException("not found no");
+    }
+
+    public void modify(HeroNode node) {
+        HeroNode temp = find(node.getNo());
+
+        temp.setName(node.getName());
+        temp.setNickName(node.nickName);
+    }
+
+    static class HeroNode {
+
+        private int no;
+
+        private String name;
+
+        private String nickName;
+
+        private HeroNode next;
+
+        public HeroNode(int no, String name, String nickName) {
+            this.no = no;
+            this.name = name;
+            this.nickName = nickName;
+        }
+
+        public int getNo() {
+            return no;
+        }
+
+        public void setNo(int no) {
+            this.no = no;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getNickName() {
+            return nickName;
+        }
+
+        public void setNickName(String nickName) {
+            this.nickName = nickName;
+        }
+
+        public HeroNode getNext() {
+            return next;
+        }
+
+        public void setNext(HeroNode next) {
+            this.next = next;
+        }
+
+        public String toString() {
+            return "HeroNode{ no=" + no + ", name=" + name + ", nickName= " + nickName + "}";
+        }
+    }
+
+    public void show() {
+        HeroNode temp = this.head;
+        while (temp.next != null) {
+            System.out.println(temp.next);
+            temp = temp.next;
+        }
+    }
+}
